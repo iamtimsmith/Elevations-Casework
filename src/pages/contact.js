627ab2f0f3655d5d3790d87d6b121ec868ad5cb8
Tim Smith <timsmith@Tims-MacBook-Pro.local>
2019-10-24 20:44:09 -0500

    Updated contact form to use netlify forms.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,37 +7,37 @@ export default () => (
     <Header />
     <div className='container content' id='content'>
       <section className='section'>
-        <h1 className='has-text-centered' style={{marginBottom:`50px`}}>Contact Us for a Quote!</h1>
+        <h1 className='has-text-centered' style={{ marginBottom: `50px` }}>Contact Us for a Quote!</h1>
         <div className='columns'>
           <div className='column'>
-          <iframe name="votar" style={{display:'none'}}></iframe>
-    <form action="http://www.elevationscasework.com/mailer.php" method="post" target="votar" id='contact'>
-            <p id='formResponse' style={{color:'green', opacity:'0', transition:'opacity .25s', textAlign:'center'}}>Thanks for reaching out to us. We'll be in contact as soon as possible!</p>
-              <input type="text" name="name" placeholder="Name" style={{width:`100%`, padding:`5px`, fontSize:`14px`, marginBottom:`10px`}}/>
-              <input type="text" name="email" placeholder="Email" style={{width:`100%`, padding:`5px`, fontSize:`14px`, marginBottom:`10px`}}/>
-              <input type="text" name="subject" placeholder="Subject" style={{width:`100%`, padding:`5px`, fontSize:`14px`, marginBottom:`10px`}}/>
-              <p style={{display:'none'}}>Empty: <input type="text" name="url" /></p>
-              <textarea name="message" placeholder="Message" style={{width:`100%`, padding:`5px`, fontSize:`14px`, marginBottom:`10px`}}></textarea>
+            <iframe name="votar" style={{ display: 'none' }}></iframe>
+            <form method="post" target="votar" id='contact' netlify-honeypot="url" data-netlify="true">
+              <p id='formResponse' style={{ color: 'green', opacity: '0', transition: 'opacity .25s', textAlign: 'center' }}>Thanks for reaching out to us. We'll be in contact as soon as possible!</p>
+              <input type="text" name="name" placeholder="Name" style={{ width: `100%`, padding: `5px`, fontSize: `14px`, marginBottom: `10px` }} />
+              <input type="text" name="email" placeholder="Email" style={{ width: `100%`, padding: `5px`, fontSize: `14px`, marginBottom: `10px` }} />
+              <input type="text" name="subject" placeholder="Subject" style={{ width: `100%`, padding: `5px`, fontSize: `14px`, marginBottom: `10px` }} />
+              <p style={{ display: 'none' }}>Empty: <input type="text" name="url" /></p>
+              <textarea name="message" placeholder="Message" style={{ width: `100%`, padding: `5px`, fontSize: `14px`, marginBottom: `10px` }}></textarea>
               <input type="submit" name="submit" value="Send" onClick={formSumbit} className='button' />
             </form>
           </div>
           <div className='column'>
-            <img src={Logo} alt="Elevations Casework" style={{margin:`0 auto`, display:`block`}}/>
-            <p style={{margin:`0`}} className='has-text-centered'>7640 Louis Rich Court</p>
-            <p style={{margin:`0`}} className='has-text-centered'>Davenport, IA 52804</p>
-            <p style={{margin:`0`}} className='has-text-centered'>Phone: 563-381-9062</p>
-            <br/>
-            <p style={{margin:`0`}} className='has-text-centered'>Scott: <a href="mailto:scott@elevationscasework.com">scott@elevationscasework.com</a></p>
-            <p style={{margin:`0`}} className='has-text-centered'>Hours of Operation: 7:00am – 3:30pm</p>
+            <img src={Logo} alt="Elevations Casework" style={{ margin: `0 auto`, display: `block` }} />
+            <p style={{ margin: `0` }} className='has-text-centered'>7640 Louis Rich Court</p>
+            <p style={{ margin: `0` }} className='has-text-centered'>Davenport, IA 52804</p>
+            <p style={{ margin: `0` }} className='has-text-centered'>Phone: 563-381-9062</p>
+            <br />
+            <p style={{ margin: `0` }} className='has-text-centered'>Scott: <a href="mailto:scott@elevationscasework.com">scott@elevationscasework.com</a></p>
+            <p style={{ margin: `0` }} className='has-text-centered'>Hours of Operation: 7:00am – 3:30pm</p>
           </div>
         </div>
       </section>
     </div>
-    </div>
+  </div>
 )
 
 const formSumbit = () => {
-  setTimeout( () => {
+  setTimeout(() => {
     document.getElementById('contact').reset()
     document.getElementById('formResponse').style.opacity = '1'
   }, 1000)
